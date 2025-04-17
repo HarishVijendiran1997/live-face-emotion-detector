9cd34657147d3930d80b8af9e41bd9b2da98195d
HarishVijendiran97 <harish.vijendiran@gmail.com>
2025-04-17 09:43:12 +0530

    add: live webcam streaming

--- src/components/EmotionDetector.jsx
+++ src/components/EmotionDetector.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 const EmotionDetector = () => {
-  return (
-    <div>EmotionDetector</div>
-  )
+
+    const videoRef = useRef(null)
+
+    useEffect(() => {
+        const startVideo = async () => {
+            try {
+                const stream = await navigator.mediaDevices.getUserMedia({ video: true })
+                videoRef.current.srcObject = stream
+            } catch (error) {
+                console.error("Error accessing webcam", error);
+            }
+        }
+
+        startVideo()
+    }, [])
+
+
+
+    return (
+        <div className="flex justify-center items-center h-screen bg-gray-900">
+            <video
+                ref={videoRef}
+                autoPlay
+                muted
+                className="rounded-lg shadow-lg border-4 border-white"
+                width="640"
+                height="480"
+            />
+        </div>
+    );
 }
 
 export default EmotionDetector
