d49d76877a05418280ac441d005a4893abbf4988
HarishVijendiran97 <harish.vijendiran@gmail.com>
2025-04-17 10:05:32 +0530

    add: detects face in the live stream

--- src/components/EmotionDetector.jsx
+++ src/components/EmotionDetector.jsx
@@ -1,26 +1,55 @@
 import React, { useEffect, useRef } from 'react'
+import * as faceapi from 'face-api.js';
 
 const EmotionDetector = () => {
 
     const videoRef = useRef(null)
+    const canvasRef = useRef(null);
+
+    //Starting webcam
+    const startVideo = async () => {
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({ video: true })
+            videoRef.current.srcObject = stream
+        } catch (error) {
+            console.error("Error accessing webcam", error);
+        }
+    }
+
+    //loading the face detection models
+    const loadModels = async () => {
+        const MODEL_URL = '/models';
+
+        try {
+            console.log("📦 Loading models...");
+            await Promise.all([faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL), faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL)])
+            console.log("✅ All models loaded successfully");
+        } catch (error) {
+            console.error("❌ Error loading models:", err);
+        }
+    }
+
+    //Detecting face in the live stream
+    const detectFace = async () => {
+        const detections = await faceapi.detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions())
+        console.log("🔍 Detected faces:", detections);
+    }
 
     useEffect(() => {
-        const startVideo = async () => {
-            try {
-                const stream = await navigator.mediaDevices.getUserMedia({ video: true })
-                videoRef.current.srcObject = stream
-            } catch (error) {
-                console.error("Error accessing webcam", error);
-            }
-        }
-
+        loadModels()
         startVideo()
+        const interval = setInterval(() => {
+            detectFace()
+        }, 2000)
+
+        return () => clearInterval(interval)
     }, [])
 
 
 
     return (
-        <div className="flex justify-center items-center h-screen bg-gray-900">
+        <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
+            <h1 className="text-2xl mb-4">🎥 Live Face Emotion Detector</h1>
             <video
                 ref={videoRef}
                 autoPlay
@@ -28,6 +57,10 @@ const EmotionDetector = () => {
                 className="rounded-lg shadow-lg border-4 border-white"
                 width="640"
                 height="480"
+            />
+            <canvas
+                ref={canvasRef}
+                style={{ position: "absolute", top: 0, left: 0 }}
             />
         </div>
     );
